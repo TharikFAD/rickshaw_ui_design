514dd1f41cf2b25ac01f1c73b4c9b9929e123e5a
Fasil Marshooq <fasil@dev-bee.com>
2023-04-06 15:10:55 +0530

    history page has been redesigned

--- lib/pages/historyPage/history_page.dart
+++ lib/pages/historyPage/history_page.dart
@@ -3,8 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meter_app/api/trip_api.dart';
+import 'package:meter_app/main.dart';
 import 'package:meter_app/model/trip_history.dart';
 import 'package:meter_app/pages/homePage/widgets/drawer.dart';
+import 'package:system_alert_window/system_alert_window.dart';
 
 import '../../routes/route_name.dart';
 
@@ -20,12 +22,20 @@ class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+
+    String tripDate = "Tue, Apr 4, 2023 17:38:16";
+    String time = "00:40:55";
+    String distance = "16.80";
+    String total = "175.78";
     return Scaffold(
       //AppBar
       appBar: AppBar(
         title: Text(
           'TRIP HISTORY',
-          style: GoogleFonts.bungee(fontSize: 22, fontWeight: FontWeight.w400),
+          style: GoogleFonts.bungee(
+            fontSize: 22,
+            //fontWeight: FontWeight.BOLD,
+          ),
         ), //appbar title
         backgroundColor: Color(0xFF4885ED), //appbar background color
 
@@ -68,7 +78,6 @@ class _HistoryPageState extends State<HistoryPage> {
                     right: 14.0,
                   ),
                   child: Container(
-                    height: size.height * 0.25,
                     width: size.width,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(12),
@@ -94,102 +103,205 @@ class _HistoryPageState extends State<HistoryPage> {
                     child: Center(
                       child: Column(
                         children: [
+//Old contents
+
+                          // Padding(
+                          //   padding: const EdgeInsets.only(left: 12.0, top: 12),
+                          //   child: Row(
+                          //     children: [
+                          //       Image.asset('assets/Driving.png'),
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       Text(
+                          //         'Date :',
+                          //         style: GoogleFonts.inter(
+                          //           fontSize: 16,
+                          //           fontWeight: FontWeight.w700,
+                          //         ),
+                          //       ),
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       Text(
+                          //         res.result![index]!.tripDate!,
+                          //         style: GoogleFonts.inter(
+                          //           fontSize: 14,
+                          //           fontWeight: FontWeight.w700,
+                          //         ),
+                          //       ),
+                          //     ],
+                          //   ),
+                          // ),
+                          // Padding(
+                          //   padding: const EdgeInsets.only(left: 12.0, top: 12),
+                          //   child: Row(
+                          //     children: [
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       Image.asset('assets/reddot.png'),
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       Text(
+                          //         'Pickup :',
+                          //         style: GoogleFonts.inter(
+                          //           fontSize: 14,
+                          //           fontWeight: FontWeight.w700,
+                          //         ),
+                          //       ),
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       Flexible(
+                          //         child: Text(
+                          //           res.result![index]!.pickupAddress!,
+                          //           overflow: TextOverflow.fade,
+                          //           style: GoogleFonts.inter(
+                          //             fontSize: 14,
+                          //             fontWeight: FontWeight.w700,
+                          //           ),
+                          //         ),
+                          //       ),
+                          //     ],
+                          //   ),
+                          // ),
+                          // Padding(
+                          //   padding: const EdgeInsets.only(left: 12.0, top: 12),
+                          //   child: Row(
+                          //     children: [
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       Image.asset('assets/greendot.png'),
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       Text(
+                          //         'Drop :',
+                          //         style: GoogleFonts.inter(
+                          //           fontSize: 14,
+                          //           //fontWeight: FontWeight.w700,
+                          //         ),
+                          //       ),
+                          //       SizedBox(
+                          //         width: size.width * 0.02,
+                          //       ),
+                          //       // Flexible(
+                          //       //   child: Text(
+                          //       //     res.result![index]!.dropAddress!,
+                          //       //     overflow: TextOverflow.fade,
+                          //       //     style: GoogleFonts.inter(
+                          //       //       fontSize: 14,
+                          //       //       fontWeight: FontWeight.w700,
+                          //       //     ),
+                          //       //   ),
+                          //       // ),
+                          //     ],
+                          //   ),
+                          // ),
+
+//New Contents starts from here!
+
                           Padding(
-                            padding: const EdgeInsets.only(left: 12.0, top: 12),
+                            padding: const EdgeInsets.all(8.0),
                             child: Row(
                               children: [
-                                Image.asset('assets/Driving.png'),
-                                SizedBox(
-                                  width: size.width * 0.02,
-                                ),
                                 Text(
-                                  'Date :',
-                                  style: GoogleFonts.inter(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: size.width * 0.02,
-                                ),
-                                Text(
-                                  res.result![index]!.tripDate!,
-                                  style: GoogleFonts.inter(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
+                                  tripDate,
+                                  style: GoogleFonts.inter(fontSize: 18),
+                                )
                               ],
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 12.0, top: 12),
-                            child: Row(
-                              children: [
-                                SizedBox(
-                                  width: size.width * 0.02,
-                                ),
-                                Image.asset('assets/reddot.png'),
-                                SizedBox(
-                                  width: size.width * 0.02,
-                                ),
-                                Text(
-                                  'Pickup :',
-                                  style: GoogleFonts.inter(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: size.width * 0.02,
-                                ),
-                                Flexible(
-                                  child: Text(
-                                    res.result![index]!.pickupAddress!,
-                                    overflow: TextOverflow.fade,
-                                    style: GoogleFonts.inter(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+                          SizedBox(
+                            height: size.height * 0.03,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 12.0, top: 12),
-                            child: Row(
-                              children: [
-                                SizedBox(
-                                  width: size.width * 0.02,
-                                ),
-                                Image.asset('assets/greendot.png'),
-                                SizedBox(
-                                  width: size.width * 0.02,
-                                ),
-                                Text(
-                                  'Drop :',
+
+//Headings
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              Container(
+                                child: Text(
+                                  "Time",
                                   style: GoogleFonts.inter(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700,
-                                  ),
+                                      fontSize: 16,
+                                      color: Colors.grey.shade600),
                                 ),
-                                SizedBox(
-                                  width: size.width * 0.02,
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              Container(
+                                child: Text(
+                                  "distance",
+                                  style: GoogleFonts.inter(
+                                      fontSize: 16,
+                                      color: Colors.grey.shade600),
                                 ),
-                                Flexible(
-                                  child: Text(
-                                    res.result![index]!.dropAddress!,
-                                    overflow: TextOverflow.fade,
-                                    style: GoogleFonts.inter(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                  ),
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                              Container(
+                                child: Text(
+                                  "Total",
+                                  style: GoogleFonts.inter(
+                                      fontSize: 16, color: Colors.grey.shade600
+
+                                      //fontWeight: FontWeight.w700),
+                                      ),
                                 ),
-                              ],
-                            ),
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                            ],
                           ),
+                          SizedBox(
+                            height: size.height * 0.02,
+                          ),
+
+//Contents
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: size.width * 0.05,
+                              ),
+                              Text(
+                                time,
+                                style: GoogleFonts.inter(fontSize: 22),
+                              ),
+                              SizedBox(
+                                width: size.width * 0.08,
+                              ),
+                              Text(
+                                "$distance KM",
+                                style: GoogleFonts.inter(fontSize: 22),
+                              ),
+                              SizedBox(
+                                width: size.width * 0.1,
+                              ),
+                              Text(
+                                total,
+                                style: GoogleFonts.inter(
+                                  fontSize: 22,
+
+                                  //fontWeight: FontWeight.w700),
+                                ),
+                              ),
+                              SizedBox(
+                                width: size.width * 0.02,
+                              ),
+                            ],
+                          ),
+                          Divider(),
+//Report Button
                           Padding(
                             padding: const EdgeInsets.only(left: 12.0, top: 12),
                             child: Row(
@@ -210,6 +322,9 @@ class _HistoryPageState extends State<HistoryPage> {
                               ],
                             ),
                           ),
+                          SizedBox(
+                            height: size.height * 0.02,
+                          ),
                         ],
                       ),
                     ),
@@ -219,7 +334,6 @@ class _HistoryPageState extends State<HistoryPage> {
             );
           }
         },
-
       ),
     );
   }
